1801c11c95f94e7dad354c844cc2d01182a5b904
Oli <oliverdernie1@googlemail.com>
2022-01-31 12:48:17 +0000

    Initial commit

---
+++ tqbc-backend/src/main/java/mygroup/tqbcbackend/TqbcBackendApplication.java
@@ -0,0 +1,13 @@
+package mygroup.tqbcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqbcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqbcBackendApplication.class, args);
+	}
+
+}
---
+++ tqbc-backend/src/test/java/mygroup/tqbcbackend/TqbcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mygroup.tqbcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqbcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
