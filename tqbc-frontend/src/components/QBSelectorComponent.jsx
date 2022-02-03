f69f6e1655fab9a4e73eb6a285acf80339af6308
Oli <oliverdernie1@googlemail.com>
2022-02-03 21:51:24 +0000

    Added Washington Commanders name and logo

---
+++ tqbc-frontend/src/components/QBSelectorComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TeamService from '../services/TeamService';
+
+class QBSelectorComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+
+    }
+}
