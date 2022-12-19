fe2228cf853514bdaa0bc4063e2033a515ac6e6b
Oli Dernie <oliverdernie1@googlemail.com>
2022-12-19 22:59:02 +0000

    Started working on create new priv leaderboard page

--- tqbc-frontend/src/components/leaderboards/CreateNewPrivateLeaderboard.js
+++ tqbc-frontend/src/components/leaderboards/CreateNewPrivateLeaderboard.js
@@ -1,3 +1,22 @@
+import { useRef } from "react";
+import Form from "react-validation/build/form";
+
 export const CreateNewPrivateLeaderboard = () => {
-    return null;
+
+    const form = useRef();
+
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <div>
+            <Form
+                onSubmit={handleSubmit}
+                ref={form}
+            >
+
+            </Form>
+        </div>
+    );
 }
