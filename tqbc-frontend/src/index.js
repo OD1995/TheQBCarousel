4b4e20df48cf7e6877d4f25d7943169e03f3cd7a
Oli Dernie <oliverdernie1@googlemail.com>
2023-01-14 21:27:54 +0000

    Second commit

--- tqbc-frontend/src/index.js
+++ tqbc-frontend/src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./store";
 import './index.css';
-import App from './App';
+import DesktopApp from './DesktopApp';
+import MobileApp from './MobileApp';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
 ReactDOM.render(
 	<Provider store={store}>
-		{/* <BrowserRouter history={history}> */}
-			<App/>
-		{/* </BrowserRouter> */}
+			{
+				(isMobile) ? <MobileApp/> : <DesktopApp/>
+			}
+			{/* {
+				(!isMobile) 
+			} */}
 	</Provider>,
 	document.getElementById("root")
 )
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
